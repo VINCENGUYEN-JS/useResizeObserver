421539936f268aaa077787d42e354ad896772fa0
VinceNguyen <nguyenquangv95@gmail.com>
2021-12-19 12:18:18 +0700

    add-use-resize-observer

---
+++ src/useResizeObserver.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useRef } from "react";
+
+type ResizeFnc = (r: ResizeObserverEntry) => void;
+
+let observer: ResizeObserver;
+
+const callBackMap = new Map<Element, ResizeFnc[]>();
+
+function init() {
+  observer = new ResizeObserver((entries) => {
+    entries.forEach((entry) => {
+      const callbacks = callBackMap.get(entry.target);
+      if (callbacks) {
+        callbacks.forEach((cb) => cb(entry));
+      }
+    });
+  });
+}
+
+export function useResizeObserver(
+  node: Element | undefined,
+  callback: ResizeFnc
+) {
+  const callbackRef = useRef<ResizeFnc>(() => void 0);
+
+  callbackRef.current = callback;
+
+  useEffect(init, []);
+
+  useEffect(() => {
+    /**
+     * If there's already an existing node, then add cb to callbackMap
+     */
+    if (node) {
+      const maybeCallbacks = callBackMap.get(node);
+      /**
+       * Defensive code
+       */
+      const safeMaybeCallbacks = maybeCallbacks || [];
+      const newCallbacks = safeMaybeCallbacks.concat(callbackRef);
+      callBackMap.set(node, newCallbacks);
+      observer.observe(node);
+    }
+
+    return () => {
+      if (node) {
+        const maybeCallbacks = callBackMap.get(node);
+        /**
+         * Defensive code
+         */
+        const safeMaybeCallbacks = maybeCallbacks || [];
+        const newCallbacks = safeMaybeCallbacks.filter(
+          (cb) => cb !== callbackRef
+        );
+        callBackMap.set(node, newCallbacks);
+        if (newCallbacks.length === 0) {
+          observer.unobserve(node);
+        }
+      }
+    };
+  });
+}
